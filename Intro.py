57cf1b651bebb40aaad6314dc81bff072775dbb3
Brian Conley <78391712+OG-Vintage1000@users.noreply.github.com>
2026-05-14 19:09:17 -0500

    Adding Python Lab 1 - Task 1 and 2

--- Intro.py
+++ Intro.py
@@ -1,8 +1,7 @@
 """ 
 Lab 1
 1. Computer programming is a discipline where written code is turned into a program that solves problems, completes process, etc. 
-2. Python is a programming language. It is popular because it is easy to read, use, and simple to use. It is extremely versatile and has
-   good support
+2. Python is a programming language. It is popular because it is easy to read, use, and simple to use.
 3. I have a choice of using VSCode IDE with a Python extension or download the terminal directly from the Python website.
 
 Lab 2
